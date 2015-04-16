51372670ff3fca1cd481c9360ac09a3736818897
Marcio Ribeiro <marcio@loggi.com>
2015-04-16 12:42:24 -0300

    [add] Initial version tralala

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+'use strict';
+var numTralalas = Number(process.argv[2]);
+var tralala = 'Tralala';
+for (var i = 0; i < numTralalas; i++) {
+  tralala += 'la';
+}
+console.log(tralala);
