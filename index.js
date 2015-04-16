fd5bca3cf4d61a28a1c90c78aa75d2f30ddc1de2
Marcio Ribeiro <marcio@loggi.com>
2015-04-16 13:09:46 -0300

    [add] 1) initial tralala, 2) random colors, 3) param vogal

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
-var numTralalas = Number(process.argv[2]);
-var tralala = '\033[31mTralala';
-for (var i = 0; i < numTralalas; i++) {
-  tralala += '\033[' + (30 + (i % 7)) + 'mla';
+var numTralalas = Number(process.argv[2]) || 0;
+var vogal = process.argv[3] || 'a';
+var initialLalas = 2;
+
+function randomInt(low, high) {
+  return Math.floor(Math.random() * (high -low + 1) + low);
+}
+
+var initialColor = randomInt(30, 37);
+
+var tralala = '\033[' + initialColor + 'mTr' + vogal;
+for (var i = 0; i < numTralalas + initialLalas; i++) {
+  tralala += '\033[' + randomInt(30, 37) + 'ml' + vogal;
 }
 console.log(tralala + '\033[0m');
