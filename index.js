4294799f57efefb46612ad1e09b6981935c3f0d1
Marcio Ribeiro <marcio@loggi.com>
2015-04-16 12:48:49 -0300

    [add] with lasers

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-'use strict';
 var numTralalas = Number(process.argv[2]);
-var tralala = 'Tralala';
+var tralala = '\033[31mTralala';
 for (var i = 0; i < numTralalas; i++) {
-  tralala += 'la';
+  tralala += '\033[' + (30 + (i % 7)) + 'mla';
 }
-console.log(tralala);
+console.log(tralala + '\033[0m');
